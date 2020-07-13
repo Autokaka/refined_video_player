7518b30c944934c19b690622286d6ddaf41cf144
Autokaka <qq1909698494@gmail.com>
2020-07-13 17:46:58 +0800

    [M] Tried to fix plugin compiler error for flutter stable branch

--- lib/src/video_player.dart
+++ lib/src/video_player.dart
@@ -443,10 +443,10 @@ class _RefinedVideoPlayerState extends State<RefinedVideoPlayer> {
               activeTrackColor: Colors.white,
               inactiveTrackColor: Colors.white24,
               thumbColor: Colors.white,
-              thumbShape: RoundSliderThumbShape(
-                enabledThumbRadius: 6,
-                pressedElevation: 10,
-              ),
+              // thumbShape: RoundSliderThumbShape(
+              //   enabledThumbRadius: 6,
+              //   pressedElevation: 10,
+              // ),
               valueIndicatorShape: PaddleSliderValueIndicatorShape(),
               trackHeight: isFullScreen ? 2 : 8,
             ),
